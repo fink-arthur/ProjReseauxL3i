fc1da5c906d595ec68db8091e54497fc7eb576c9
Sargon83 <fink.arthur@gmail.com>
2015-04-11 17:45:07 +0200

    Ajout du stdin

--- Server.py
+++ Server.py
@@ -3,6 +3,7 @@
 import socket               # Import socket module
 import random
 from thread import start_new_thread
+from TTH import TTH
 
 # Mise en place du serveur
 s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)         # Create a socket object
@@ -18,14 +19,19 @@ def initDict(chemin) :
         array.append(line)
   return array
 
-liste = initListe('dict.txt')
-
 def boucleinterne(mot):
   liste = []
   for j in range(1,27):
       motinter = mot
       liste.append(mot + chr(96 + j))
   return liste
+
+def inputthread():
+  while True:
+    string = raw_input("Entrer une chaine de caracteres: ")
+    res = TTH((0,0,0,0),string)
+    print(res)
+
 
 def clientthread(c):
   global compteur
@@ -34,14 +40,19 @@ def clientthread(c):
      if(msg == "GET\n"):
          c.sendall("SET " + liste[compteur] + "\n")
          compteur += 1
-         print("GET envoyer")
+         #print("GET envoyer")
          msg = c.recv(1024).decode('UTF-8')
          if (msg[:6] == "RETURN"):
-            print(msg)
+            #print(msg)
+            pass
 
-# Ecoute sur le socket
-s.listen(5)                 # Now wait for client connection.
-while (True):
-  c, addr = s.accept()     # Establish connection with client.
-  print ('Got connection from', addr)
-  start_new_thread(clientthread, (c,)) # dedicasse a Julien
+
+if __name__ == "__main__":
+  liste = initDict('dict.txt')
+  # Ecoute sur le socket
+  start_new_thread(inputthread,())
+  s.listen(5)                 # Now wait for client connection.
+  while (True):
+    c, addr = s.accept()     # Establish connection with client.
+    print ('Got connection from', addr)
+    start_new_thread(clientthread, (c,)) # dedicasse a Julien
