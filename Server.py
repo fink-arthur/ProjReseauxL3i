6274235cb25d5e71febe4ed1e765626cec620dc6
Sargon83 <fink.arthur@gmail.com>
2015-04-11 17:18:24 +0200

    On continue le projet

--- Server.py
+++ Server.py
@@ -2,30 +2,46 @@
 
 import socket               # Import socket module
 import random
+from thread import start_new_thread
 
 # Mise en place du serveur
 s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 2048                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
+compteur = 0
 
+def initDict(chemin) :
+  with open(chemin) as ins:
+    array = []
+    for line in ins:
+        array.append(line)
+  return array
 
-# Ecoute sur le socket
-s.listen(5)                 # Now wait for client connection.
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print ('Got connection from', addr)
-   msg = c.recv(1024).decode('UTF-8')
-   if(msg == "GET\n"):
-      if (random.randint(0,2) == 2):
-         c.sendall("NOPE " + "plus de travail\n")
-         print("Nope envoier")
-      else:
-         c.sendall("SET " + "AYHGD\n")
+liste = initListe('dict.txt')
+
+def boucleinterne(mot):
+  liste = []
+  for j in range(1,27):
+      motinter = mot
+      liste.append(mot + chr(96 + j))
+  return liste
+
+def clientthread(c):
+  global compteur
+  while True:
+     msg = c.recv(1024).decode('UTF-8')
+     if(msg == "GET\n"):
+         c.sendall("SET " + liste[compteur] + "\n")
+         compteur += 1
          print("GET envoyer")
          msg = c.recv(1024).decode('UTF-8')
          if (msg[:6] == "RETURN"):
             print(msg)
-         
-      
-   c.close()                # Close the connection
+
+# Ecoute sur le socket
+s.listen(5)                 # Now wait for client connection.
+while (True):
+  c, addr = s.accept()     # Establish connection with client.
+  print ('Got connection from', addr)
+  start_new_thread(clientthread, (c,)) # dedicasse a Julien
--- client.py
+++ client.py
@@ -4,33 +4,28 @@ import socket               # Import socket module
 import time
 from TTH import TTH
 
-while (True):
-    # Connection au serveur
-    s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)         # Create a socket object
-    host = socket.gethostname() # Get local machine name
-    port = 2048                # Reserve a port for your service.
-    s.connect((host, port))
+# Connection au serveur
+s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 2048                # Reserve a port for your service.
+s.connect((host, port))
 
-
+while(True):
     # Le client envoie GET au serveur
     s.sendall("GET\n")
 
     # Le client recoit son SET et la chaine a hasher et ferme la connexion
     msg = s.recv(1024).decode('UTF-8')
     
-    if (msg == "NOPE plus de travail\n"):
-    	exit(0)
-    elif (msg[:4] == "NOPE"):
-        pass
+    if (msg == "NOPE 1\n"):
+    	time.sleep(60)
+    elif (msg == "NOPE 2\n"):
+        s.close()
+        exit(0)
     else:
 	    # Le client effectue le travail
-	    msg = msg.split(" ")[1]
-	    res = TTH((0,0,0,0),msg)
-
+        #print(msg)
+        msg = msg.split(" ")[1]
+        res = TTH((0,0,0,0),msg)
 	    ## le client envoie le RETURN
-	    s.sendall("RETURN " + msg + " " + res + "\n")
-	    s.close()                     # Close the socket when done
-	    time.sleep(5)
-
-# from http://www.tutorialspoint.com/python/python_networking.htm
-# autre ref: http://docs.python.org/3.1/howto/sockets.html
+        s.sendall("RETURN " + msg + " " + res + "\n")
