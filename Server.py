eb98ba0bbc37d9f7bff5ac8e17ef5d9c5147d871
Sargon83 <fink.arthur@gmail.com>
2015-04-18 15:57:00 +0200

    il est beau le serveur

--- Server.py
+++ Server.py
@@ -1,76 +1,130 @@
 #!/usr/local/bin/python3    # This is server.py file
 
-import socket               # Import socket module
+import socket
 import random
 from thread import start_new_thread
 from TTH import TTH
 import sys, getopt
 from baseDeDonnee import *
 
-def initDict(chemin) :
+def initDict(chemin):
+   """
+   Lis tous les lignes d'un fichier a l'adresse chemin et les mets dans un array
+   """
    with open(chemin) as ins:
       array = []
       for line in ins:
          array.append(line.rstrip())    
    return array
-
-def boucleinterne(mot):
-   liste = []
-   for j in range(1,27):
-      motinter = mot
-      liste.append(mot + chr(96 + j))
-   return liste
        
 def DIC():
-    liste = []
-    for i in range(26):
+   """
+   Cree tous les mots de 4 lettres et les met dans un array
+   """
+   liste = []
+   for i in range(26):
 	for j in range(26):
 		for k in range(26):
 			for l in range(26):
 				liste.append(chr(i+97)+chr(j+97)+chr(k+97)+chr(l+97))
-    return liste
+   return liste
                       
 def inputthread():
+   """
+   Thread qui attends un input (qui va passer dans TTH) de l'utilisateur pour en chercher les collisions
+   """
    while True:
       string = raw_input("Entrer une chaine de caracteres: ")
       res = TTH((0,0,0,0),string)
       rechercheCollision(res)
      
 def rechercheCollision(mot):
-    listeCollision = recherche(mot)
-    print("COLLISION de "+mot+" :"+str(listeCollision))
+   """
+   Prend un tth et renvoie tous les mots de la BDD avec le meme tth
+   """
+   listeCollision = recherche(mot)
+   print("COLLISION de "+mot+" :"+str(listeCollision))
 
 def clientthread(c):
+   """
+   Thread qui va gerer la connection avec un client
+   """
    global iterateur
    carryover = ""
+   travail = ""
+
+
+   ########################################
+   #                                      #
+   #      Mise en place du client         #
+   #                                      #
+   ########################################
+
    while True:
+
+      # On regarde si l'envoie du get par le client n'as pas ete trop rapide
       if (carryover == ""):
          msg = c.recv(1024).decode('UTF-8')
       else:
          msg = carryover + "\n"
          carryover = ""
+
+      # On regarde si le premier message est bien GET
       if(msg == "GET\n"):
+
+         # On regarde si l'iterateur qui nous donne le travail est vide
          try:
             travail = iterateur.next()
          except StopIteration:
-            iterateur = rechercheTravail().__iter__()
-            travail = iterateur.next()
+            recherche = rechercheTravail()
+            if (len(recherche) == 0): # Plus de travail a faire dans la BDD
+               iterateur = None
+            iterateur = recherche.__iter__()
 
-         c.sendall("SET " + travail + "\n")
-         msg = c.recv(1024).decode('UTF-8')
-         if (msg[:6] == "RETURN"):
-            acc = msg.rstrip().split(" ")
-           # print(port)
-            returnTravail(acc[1], acc[2])
-            if (len(msg.split("\n")) == 3):
-               carryover = msg.split("\n")[1]
-          #  print(msg)
+
+         if (iterateur == None): # On endort le client car plus de travail
+            c.sendall("NOPE 1\n")
+         else:
+
+            # On donne le travail
+            if (travail == ""):
+               travail = iterateur.next()
+            c.sendall("SET " + travail[0] + "\n")
+            travail = ""
+
+            # On recoit normalement le RETURN du client
+            msg = c.recv(1024).decode('UTF-8')
+            if (msg[:6] == "RETURN"):
+               acc = msg.rstrip().split(" ")
+               returnTravail(acc[1], acc[2])
+               if (len(msg.split("\n")) == 3):
+                  carryover = msg.split("\n")[1]
+               #print(msg)
+
+            # Le message n'est pas RETURN
+            else :
+               c.sendall("NOPE 2\n")
+               print(msg)
+               c.close()
+
+      # Le premier message n'est pas GET mais autre chose
+      else :
+         c.sendall("NOPE 2\n")
+         print(msg)
+         c.close()
 
 if __name__ == "__main__":
 
    port = 2048                # Reserve a port for your service.
-   nbClient=5                  # number of max clients
-   liste = initDict('dict.txt')
+   nbClient = 5                  # number of max clients
+   liste = None
+
+   ########################################
+   #                                      #
+   #        On gere les options           #
+   #                                      #
+   ########################################
+
    try:
       opts, args = getopt.getopt(sys.argv[1:],"hp:c:f:",["parg=","carg=","farg="])
    except getopt.GetoptError:
@@ -84,33 +138,49 @@ if __name__ == "__main__":
       elif opt in ("-p", "--parg"):
          port = int(arg)
       elif opt in ("-c", "--carg"):
-         nbClient=arg
+         nbClient = int(arg)
       elif opt in ("-f", "--farg"):
          liste = initDict(arg)
 
-   # Mise en place du serveur
-   s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)         # Create a socket object
-   host = socket.gethostname() # Get local machine name
-   s.bind((host, port))        # Bind to the port
+
+   ########################################
+   #                                      #
+   #        On initialise la BDD          #
+   #                                      #
+   ########################################
   
 
-   initTable()
+   t = initTable()
   
    # Ecoute sur le socket
  
-   remplissage(liste)
-
-   iterateur = liste.__iter__()
+   if (t == 0):
+      if (liste == None):
+         remplissage(DIC())
+      else:
+         remplissage(liste)
+   
+   iterateur = rechercheTravail().__iter__()
   
-   #readTable()
+
+   ########################################
+   #                                      #
+   #      Mise en place du serveur        #
+   #                                      #
+   ########################################
+
+
+   s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)   # Create a socket object
+   host = socket.gethostname()                              # Get local machine name
+   s.bind((host, port))                                     # Bind to the port
   
    start_new_thread(inputthread,())
-   s.listen(nbClient)                 # Now wait for client connection.
+   s.listen(nbClient)                  # Now wait for client connection.
    while (True):
       try:
-         c, addr = s.accept()     # Establish connection with client.
+         c, addr = s.accept()          # Establish connection with client.
          print ('Got connection from', addr)
-         start_new_thread(clientthread, (c,)) # dedicasse a Julien	
+         start_new_thread(clientthread, (c,)) # Dedicasse a Julien	
       except KeyboardInterrupt:
          s.close()
          sys.exit()
